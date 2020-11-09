83330952896dab84dd527fddfbcda5be3d1ad3b9
sijen <50243460+sijen@users.noreply.github.com>
2020-11-09 13:05:21 +0545

    final

--- script/logic.js
+++ script/logic.js
@@ -1,10 +1,16 @@
 function side_display(){
     var v = document.getElementById("nav-side-panel");
+    var a = document.querySelector("#main-panel");
+    a.style.width="80%";
+    a.style.float="right";
     v.style.left="0px";
     v.style.transition="0.3s all";
 }
 function side_hide(){
     var v = document.getElementById("nav-side-panel");
+    var a = document.querySelector("#main-panel");
+    a.style.width="100%";
+    a.style.clear="both";
     v.style.left="-60%";
     v.style.transition="0.3s all";
 }
