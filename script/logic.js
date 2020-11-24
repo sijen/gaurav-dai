1af170ab08795527ecfc4d45e88a5024dc1f7fe5
sijen <50243460+sijen@users.noreply.github.com>
2020-11-24 17:05:16 +0545

    list of products

--- script/logic.js
+++ script/logic.js
@@ -11,6 +11,6 @@ function side_hide(){
     var a = document.querySelector("#main-panel");
     a.style.width="100%";
     a.style.clear="both";
-    v.style.left="-60%";
+    v.style.left="-50%";
     v.style.transition="0.3s all";
 }
